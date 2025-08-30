b9de26cefd1ea50342172a0ca0f15b1f76cba8ce
JoshuaNicholls <95411878+JoshuaNicholls@users.noreply.github.com>
2025-08-30 15:13:26 +0800

    Add up to evidentiality dict

--- dictionary_lookup.py
+++ dictionary_lookup.py
@@ -205,6 +205,276 @@ movement_dict = {
     "-2": "?"
 }
 
+number_dict = {
+    "1": "Numerical",
+    "2": "Null",
+    "3": "Singular",
+    "5": "Dual",
+    "7": "Trial",
+    "11": "Quartal",
+    "13": "Quintal",
+    # … etc.
+    "-1": "Anumerical",
+    "-2": "Plural",
+    "-3": "Collective",
+    "-5": "Paucal",
+    "-7": "Infinitive",
+    "-11": "Greater Paucal",
+    "-13": "Greater Plural"
+}
+
+aspect_dict = {
+    "1": "Aspect Finite",
+    "2": "Simple",
+    "3": "Progressive",
+    "5": "Gnomic",
+    "7": "Perfect",
+    "11": "Infinitive",
+    "13": "Cyclical"
+}
+
+participle_dict = {
+    "1": "Finite",
+    "2": "?",
+    "-1": "Participle",
+    "-2": "Agent Particuple"
+}
+
+music_dict = {
+    "1": "?",
+    "2": "?",
+    "-1": "?",
+    "-2": "?"
+}
+
+dance_dict = {
+    "1": "?",
+    "2": "?",
+    "-1": "?",
+    "-2": "?"
+}
+
+size_dict = {
+    "1": "Small Size",
+    "2": "Indefinite",
+    "3": "Seed",
+    "5": "Pinky knuckle",
+    "7": "Pinky",
+    "11": "Tennis Ball",
+    "13": "Handspan",
+    "17": "Lower Leg",
+    "19": "Half Humanoid",
+    "-1": "Larger than Humanoid",
+    "-2": "Humanoid",
+    "-3": "1.5x Humanoid",
+    "-5": "3x Humanoid",
+    "-7": "Cathedral Interior",
+    "-11": "City",
+    "-13": "Colossal",
+    "-17": "Planet",
+    "-19": "Infinite"
+}
+
+shape_dict = {
+    "1": "Geometric Shape",
+    "2": "Undefined",
+    "3": "Line",
+    "5": "Circular",
+    "7": "Square",
+    "11": "Triangle",
+    "13": "Semicircle",
+    "17": "Spherical",
+    "19": "Cylindrical",
+    "23": "Cuboid",
+    "29": "Tetrahedral",
+    "-1": "Non-Geometric Shape",
+    "-2": "Unknown",
+    "-3": "Ovoid",
+    "-5": "Geoid",
+    "-7": "Kiki",
+    "-11": "Bouba",
+    "-13": "Grainy",
+    "-17": "Fuzzy",
+    "-19": "Mammal"
+}
+
+person_number_dict = {
+    "1": "Numeropersonal",
+    "2": "1st Singular",
+    "3": "2nd Singular",
+    "5": "3rd Singular",
+    "7": "4th Singular",
+    "11": "1st Plural",
+    "13": "2nd Plural",
+    "17": "3rd Plural",
+    "19": "4th Plural",
+    "23": "1st Dual",
+    "29": "2nd Dual",
+    "31": "3rd Dual",
+    "37": "4th Dual",
+    "41": "1st Collective",
+    "43": "2nd Collective",
+    "47": "3rd Collective",
+    "53": "4th Collective",
+    "59": "1st Paucal",
+    "61": "2nd Paucal",
+    "67": "3rd Paucal",
+    "71": "4th Paucal",
+    "73": "1st Trial",
+    "79": "2nd Trial",
+    "83": "3rd Trial",
+    "89": "4th Trial",
+    "93": "1st Infinite",
+    "101": "2nd Infinite",
+    "103": "3rd Infinite",
+    "107": "4th Infinite",
+    # + Greater Paucal / Greater Plural / Quartal placeholders here
+    "-1": "Numeroapersonal",
+    "-2": "Demonstrative Singular",
+    "-3": "Interrogative Singular",
+    "-5": "Relative Singular",
+    "-7": "Demonstrative Plural",
+    "-11": "Interrogative Plural",
+    "-13": "Relative Plural",
+    "-17": "Demonstrative Dual",
+    # … etc.
+}
+
+element_dict = {
+    "1": "Terrestrial Element",
+    "2": "Metal",
+    "3": "Wood",
+    "5": "Earth",
+    "7": "Flesh",
+    "11": "Water",
+    "-1": "Extra-Terrestrial Element",
+    "-2": "Fire",
+    "-3": "Air",
+    "-5": "Spirit",
+    "-7": "Dark Matter",
+    "-11": "Aether"
+}
+
+cardinality_dict = {
+    "1": "Non-Cardinals",
+    "2": "Fixed",
+    "3": "Mutable",
+    "-1": "Cardinals",
+    "-2": "Spacially Cardinal",
+    "-3": "Temporally Cardinal"
+}
+
+voice_dict = {
+    "1": "Positive Voice",
+    "2": "Active",
+    "3": "Passive",
+    "5": "Mediopassive",
+    "-1": "Negative Voice",
+    "-2": "Negative Active",
+    "-3": "Negative Passive",
+    "-5": "Negative Mediopassive"
+}
+
+transitivity_dict = {
+    "1": "Transitivity",
+    "2": "Causative",
+    "-1": "",
+    "-2": ""
+}
+
+animacy_dict = {
+    "1": "Animate (Living)",
+    "2": "Non-cellular life",
+    "3": "Prokaryote",
+    "5": "Unicellular Eukaryote",
+    "7": "Microscopic Fungus",
+    "11": "Microscopic Plant",
+    "13": "Macroscopic Plant",
+    "17": "Macroscopic Fungus",
+    "19": "Microscopic Animal",
+    "23": "Low Animal",
+    "29": "Mid Animal",
+    "31": "High Animal",
+    "37": "Near Sapient Animal",
+    "41": "Low Sapient",
+    "43": "High Spaient",
+    "47": "Enlightened Sapient",
+    "53": "Low Destroyer",
+    "59": "Low Creator",
+    "61": "High Destroyer",
+    "67": "High Creator",
+    "-1": "Inanimate (Non-living)",
+    "-2": "Concept",
+    "-3": "Subatomic particle",
+    "-5": "Atom",
+    "-7": "Compound",
+    "-11": "Natural Inanimate",
+    "-13": "Sapient-made Inanimate",
+    "-17": "Dead organic matter",
+    "-19": "Part of a living being",
+    "-23": "Animate natural phenomenon",
+    "-29": "Speech",
+    "-31": "Planet",
+    "-37": "Star",
+    "-41": "Nebula",
+    "-43": "Galaxy",
+    "-47": "Universe"
+}
+
+animal_type_high_dict = {
+    "1": "Medium Animal",
+    "2": "Beast",
+    "3": "Fish",
+    "5": "Small Reptile",
+    "-1": "",
+    "-2": "",
+    "-3": "",
+    "-5": ""
+}
+
+animal_small_reptile_dict = {
+    "1": "",
+    "2": "Serpent",
+    "3": "Tortoise",
+    "5": "",
+    "-1": "",
+    "-2": "",
+    "-3": "",
+    "-5": ""
+}
+
+mood_dict = {
+    "1": "Realis",
+    "2": "Indicative",
+    "3": "Imperative",
+    "5": "Necessative",
+    "7": "Declarative",
+    "11": "Admonitive",
+    "-1": "Irrealis",
+    "-2": "Optative",
+    "-3": "Subjunctive",
+    "-5": "Conditional",
+    "-7": "Presumptive",
+    "-13": "Contrafactual"
+}
+
+evidentiality_dict = {
+    "1": "Sure source",
+    "2": "Did i' meself",
+    "3": "(Visual) Saw i' wiv me own oies, oi did",
+    "5": "(Auditory) 'Eard i' wiv me own eas, oi did",
+    "7": "(Touch / ESP) Few' i' fa sure",
+    "11": "(Olfactogustatory) Smew' / tighsted i'",
+    "13": "Was jus' roun' t' corner when i' 'appened",
+    "17": "Trus'worvy frien' tol' me",
+    "19": "Reasonably reloiable source",
+    "-1": "Unsure source",
+    "-2": "Figured i' ou' based on wha' oi've seen",
+    "-3": "Sloightly sketchy source",
+    "-5": "Wouldn' trus' as far 's a could frow 'em",
+    "-7": "Guaran'ee vis did no' 'appen"
+}
+
 # Running lists
 numeric_values = []
 linguistic_properties = []
